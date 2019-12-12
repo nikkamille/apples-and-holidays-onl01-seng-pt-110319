5f8bf3f15cf68c32327dfa10567fae4a8155dca2
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-12 10:38:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize!
+    puts "#{season.capitalize}:"
     holiday.map do |holiday_name, supplies|
     end
   end
