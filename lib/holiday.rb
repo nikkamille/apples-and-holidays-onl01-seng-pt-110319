5328cca39d96f1ce1936bfa486f5f0c0c7dbf0b8
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-12 10:56:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,6 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday_name, supplies|
+      holiday_name if supplies.include?("BBQ")
 
 end
 
