1f4fe5077d677a2a92a0389bb4ae3da4e6026358
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-12 10:29:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,8 +59,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |season, holiday|
-    season.to_s 
+  holiday_hash.map do |season, holiday|
+    season.to_s.split 
+    binding.pry
   end
 end
 
