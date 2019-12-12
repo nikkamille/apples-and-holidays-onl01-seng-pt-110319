d3ccfd00b17e9937eab9f49a9851b25cb4f27fa2
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-12 10:55:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,8 +70,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holidays_with_bbq = []
-  holidays_with_bbq << holiday_hash[season]
+  holiday_hash
 
 end
 
